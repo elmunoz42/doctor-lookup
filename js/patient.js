db38dee4a946d8849e4e0db80d89c76e76898860
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-17 13:15:34 -0700

    add address features from geolocation branch.

--- js/patient.js
+++ js/patient.js
@@ -4,18 +4,5 @@ function Patient (name, symptom, location) {
   this.location = location;
 }
 
-// var currentLocation = document.getElementById("demo");
-// Patient.prototype.getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     alert("Geolocation is not supported by this browser.");
-//   }
-// }
-//
-// Patient.prototype.showPosition(position) {
-//   currentLocation.innerHTML = "Latitude: " + position.coords.latitude +
-//   "<br>Longitude: " + position.coords.longitude;
-// }
 
 exports.patientModule = Patient;
