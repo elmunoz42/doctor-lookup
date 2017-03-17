aeb298c4eb58ed23133625fbc4f0138a43835432
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-17 15:55:30 -0700

    integrate geolocation with setTimeout handling asynchrony NOTE: find better solution.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,10 +1,11 @@
 var Doctor = require('./../js/lookup.js').doctorModule;
 var Patient = require('./../js/patient.js').patientModule;
-
+var testLat=0;
+var testLon=0;
 
 var displaySuccess = function(doctor) {
   $("#recommendations").append(
-    "<tr>" + "<td>" + doctor.name + "</td>" + "<td>" + doctor.specialty + "</td>"+ "<td>" + doctor.address + "</td>" + "</tr>" 
+    "<tr>" + "<td>" + doctor.name + "</td>" + "<td>" + doctor.specialty + "</td>"+ "<td>" + doctor.address + "</td>" + "</tr>"
   );
 };
 
@@ -12,18 +13,59 @@ var displayError = function(error) {
   alert("BetterDoctor API failed to get results! " + error);
 };
 
+
+var getLocation = function() {
+  if (navigator.geolocation) {
+    currentPos = navigator.geolocation.getCurrentPosition(getPosition);
+    console.log(navigator.geolocation);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+  return currentPos;
+};
+
+var getPosition = function(position, positionFound) {
+  testLat = position.coords.latitude;
+  testLon = position.coords.longitude;
+  var currentPosition = [testLat, testLon];
+  console.log("testLat in function" + testLat);
+  console.log("testLon in function" + testLon);
+  positionFound = true;
+  return currentPosition;
+};
+
 $(document).ready(function() {
 
+  var positionFound = false;
   $("#intake-form").submit(function(){
     event.preventDefault();
     $("#recommendations").empty();
+    var location = $("#location").val();
+    console.log("type of location: " + typeof(location));
+    console.log("location: " + location);
+    var patientLat = 45.5209678;
+    var patientLon = -122.6775636;
+    var name = $("#name").val();
     var symptom = $("#symptom").val();
     var doctor_class = new Doctor("","","");
-    doctor_class.getDoctors(symptom, displaySuccess, displayError);
-
+    if (location==='0') {
+      doctor_class.getDoctors(symptom, displaySuccess, displayError, patientLat, patientLon);
+    }
+    if (location==='1') {
+        getLocation(getPosition);
+        console.log("loading...");
+        setTimeout(function(){
+          patientLat = testLat;
+          patientLon = testLon;
+          console.log("testLat in submit: " + testLat);
+          console.log("testLon in submit: " + testLon);
+          doctor_class.getDoctors(symptom, displaySuccess, displayError, patientLat, patientLon);
+        }, 6000 );
+        // timeout
+    }
+    // if
   });
-
-
-
+  // submit
 
 });
+// document ready
--- js/patient.js
+++ js/patient.js
@@ -1,21 +1,9 @@
-function Patient (name, symptom, location) {
+function Patient (name, symptom, lat, lon) {
   this.name = name;
   this.symptom = symptom;
-  this.location = location;
+  this.patientLat = lat;
+  this.patientLon = lon;
 }
 
-// var currentLocation = document.getElementById("demo");
-// Patient.prototype.getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     alert("Geolocation is not supported by this browser.");
-//   }
-// }
-//
-// Patient.prototype.showPosition(position) {
-//   currentLocation.innerHTML = "Latitude: " + position.coords.latitude +
-//   "<br>Longitude: " + position.coords.longitude;
-// }
 
 exports.patientModule = Patient;
