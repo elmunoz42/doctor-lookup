a963e9f57fb11ddc99a333e3dfd343b7d19efb19
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-17 09:33:02 -0700

    add css styling (test).

---
+++ js/lookup-interface.js
@@ -0,0 +1,10 @@
+var getDoctors = require('./../js/lookup.js').getDoctors;
+var Patient = require('./../js/patient.js').Patient;
+
+$(document).ready(function() {
+
+  var result = getDoctors('head ache');
+
+
+
+});
---
+++ js/patient.js
@@ -0,0 +1,6 @@
+function Patient (name, symptom) {
+  this.name = name;
+  this.symptom = symptom;
+}
+
+exports.patientModule = Patient;
